6f0f14cb1c3c3a5b686a72fbc2b0791ef53d5b14
iNfame <vyacheslav.anosov@adwatch.ru>
2017-03-17 20:05:55 +0300

    fixes for backend-repo + new tasks + npm run + gulpfile fixes to multiple paths + css fix for indexGenerate

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,13 +4,14 @@ import del from "del";
 import gulp from "gulp";
 import open from "open";
 import rimraf from "rimraf";
-import _ from "lodash";
 import source from "vinyl-source-stream";
 import buffer from "vinyl-buffer";
 import gulpLoadPlugins from "gulp-load-plugins";
-import packageJson from "./package.json";
 import runSequence from "run-sequence";
 import inject from "gulp-inject";
+
+import yargs from "yargs";
+const args = yargs.argv;
 
 import browserSync from "browser-sync";
 let reload = browserSync.reload;
@@ -22,23 +23,42 @@ import babelify from "babelify";
 const PORT = process.env.PORT || 3000;
 const $ = gulpLoadPlugins({camelize: true});
 
-const PUBLIC_DIR = './public';
+// images optimization flag
+const shortOptimizeImagesFlag = args["oi"];
+const longOptimizeImagesFlag = args["optimize-images"];
+const optimizeImagesFlag = shortOptimizeImagesFlag || longOptimizeImagesFlag;
+
+// local path serve flag
+const shortLocalDestPathFlag = args["l"];
+const longLocalDestPathFlag = args["locally"];
+const localDestPathFlag = shortLocalDestPathFlag || longLocalDestPathFlag;
+
+const PUBLIC_DIR = localDestPathFlag ? './public' : './web/bundles/frontend';
 const SOURCES_DIR = './sources';
 
-var sassConfig = {
+let sassConfig = {
 	sassPath: './' + SOURCES_DIR + '/css',
 	bowerDir: './bower_components'
 };
 
-var jsConfig = {
-	vendorPath: SOURCES_DIR + '/js/vendor'
+let jsConfig = {
+	vendor: {
+		srcPath: '/js/vendor',
+		destPath: '/js',
+		file: 'vendor.js'
+	},
+	custom: {
+		srcPath: '/js/custom',
+		destPath: '/js',
+		file: 'custom.js'
+	},
 };
 
 
 // Main Tasks
 gulp.task('serve', () => runSequence('serve:clean', 'serve:start', 'indexGenerate'));
-gulp.task('dist', () => runSequence('dist:clean', 'dist:build', 'web', 'indexGenerate', () => {
-	open('http://localhost:' + PORT)
+gulp.task('dist', () => runSequence('dist:clean', 'dist:build', 'indexGenerate', () => {
+
 }));
 gulp.task('auto-build', () => runSequence('dist:clean', 'dist:build'));
 gulp.task('clean', ['dist:clean, serve:clean']);
@@ -76,7 +96,7 @@ gulp.task('dist:sass', () => {
 });
 
 gulp.task('serve:pug', () => {
-	return gulp.src(SOURCES_DIR + '/*.pug')
+	return localDestPathFlag ? gulp.src(SOURCES_DIR + '/*.pug')
 		.pipe($.pug({
 			pretty: true
 		}).on('error', $.notify.onError(function (error) {
@@ -84,21 +104,31 @@ gulp.task('serve:pug', () => {
 		})))
 		.pipe($.prettify({indent_char: '	', indent_size: 1}))
 		.pipe(gulp.dest(PUBLIC_DIR + '/'))
-		.pipe(browserSync.reload({stream: true}));
+		.pipe(browserSync.reload({stream: true})) : false;
 });
 
 gulp.task('serve:vendor-js', () => {
-	return gulp.src(jsConfig.vendorPath + '/*.js')
-		.pipe($.concat('vendor.js'))
-		.on('error', $.notify.onError(function(error) {
+	return gulp.src(SOURCES_DIR + jsConfig.vendor.srcPath + '/*.js')
+		.pipe($.concat(jsConfig.vendor.file))
+		.on('error', $.notify.onError(function (error) {
 			return 'Error: ' + error.message;
 		}))
-		.pipe(gulp.dest(PUBLIC_DIR + '/js'))
+		.pipe(gulp.dest(PUBLIC_DIR + jsConfig.vendor.destPath))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('serve:custom-js', () => {
+	return gulp.src(SOURCES_DIR + jsConfig.custom.srcPath + '/*.js')
+		.pipe($.concat(jsConfig.custom.file))
+		.on('error', $.notify.onError(function (error) {
+			return 'Error: ' + error.message;
+		}))
+		.pipe(gulp.dest(PUBLIC_DIR + jsConfig.custom.destPath))
 		.pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('dist:pug', () => {
-	return gulp.src(SOURCES_DIR + '/*.pug')
+	return localDestPathFlag ? gulp.src(SOURCES_DIR + '/*.pug')
 		.pipe($.pug({
 			pretty: true
 		}).on('error', $.notify.onError(function (error) {
@@ -106,19 +136,19 @@ gulp.task('dist:pug', () => {
 		})))
 		.pipe($.prettify({indent_char: '	', indent_size: 1}))
 		.pipe(gulp.dest(PUBLIC_DIR + '/'))
-		.pipe(browserSync.reload({stream: true}));
+		.pipe(browserSync.reload({stream: true})) : false;
 });
 
 gulp.task('dist:vendor-js', () => {
-	return gulp.src(jsConfig.vendorPath + '/*.js')
-		.pipe($.concat('vendor.js')
+	return gulp.src(SOURCES_DIR + jsConfig.vendor.srcPath + '/*.js')
+		.pipe($.concat(jsConfig.vendor.file)
 			.on('error', $.notify.onError(function (error) {
 				return 'Error: ' + error.message;
 			}))
 		)
 		.pipe($.minify({
-				ext:{
-					min:'.js'
+				ext: {
+					min: '.js'
 				},
 				noSource: true
 			})
@@ -126,20 +156,41 @@ gulp.task('dist:vendor-js', () => {
 					return 'Error: ' + error.message;
 				}))
 		)
-		.pipe(gulp.dest(PUBLIC_DIR + '/js'));
+		.pipe(gulp.dest(PUBLIC_DIR + jsConfig.vendor.destPath));
 });
 
-gulp.task('serve:start', ['serve:sass', 'serve:images', 'serve:pug', 'serve:vendor-js', 'serve:static', 'watch', 'web-bs'], () => {
+gulp.task('dist:custom-js', () => {
+	return gulp.src(SOURCES_DIR + jsConfig.custom.srcPath + '/*.js')
+		.pipe($.concat(jsConfig.custom.file)
+			.on('error', $.notify.onError(function (error) {
+				return 'Error: ' + error.message;
+			}))
+		)
+		.pipe($.minify({
+				ext: {
+					min: '.js'
+				},
+				noSource: true
+			})
+				.on('error', $.notify.onError(function (error) {
+					return 'Error: ' + error.message;
+				}))
+		)
+		.pipe(gulp.dest(PUBLIC_DIR + jsConfig.custom.destPath));
+});
+
+gulp.task('serve:start', ['serve:sass', 'serve:images', 'serve:pug', 'serve:vendor-js', 'serve:custom-js', 'serve:static', 'watch', 'web-bs'], () => {
 
 });
 
-gulp.task('dist:build', ['dist:sass', 'dist:images', 'dist:pug', 'dist:static', 'lint', 'build-once', 'dist:vendor-js']);
+gulp.task('dist:build', ['dist:sass', 'dist:images', 'dist:pug', 'dist:static', 'lint', 'build-once', 'dist:vendor-js', 'dist:custom-js']);
 
 gulp.task('dist:images', () => {
 	return gulp.src([SOURCES_DIR + '/img/**/*'])
-		.pipe($.image({
-
-		})).on('error', $.notify.onError(function (error) {
+		.pipe($.if(optimizeImagesFlag, $.image({
+				// some options may pass here
+			}))
+		).on('error', $.notify.onError(function (error) {
 			return 'Error: ' + error.message;
 		}))
 		.pipe(gulp.dest(PUBLIC_DIR + '/img/'));
@@ -152,18 +203,18 @@ gulp.task('serve:images', () => {
 });
 
 gulp.task('serve:static', () => {
-	return gulp.src([SOURCES_DIR + '/static/**/*'])
+	return gulp.src([SOURCES_DIR + '/static/**/*', '!' + SOURCES_DIR + '/static/_pages.html'])
 		.pipe($.changed(PUBLIC_DIR + '/'))
 		.pipe(gulp.dest(PUBLIC_DIR + '/'))
 });
 
 gulp.task('dist:static', () => {
-	return gulp.src([SOURCES_DIR + '/static/**/*'])
+	return gulp.src([SOURCES_DIR + '/static/**/*', '!' + SOURCES_DIR + '/static/_pages.html'])
 		.pipe(gulp.dest(PUBLIC_DIR + '/'))
 });
 
 gulp.task('lint', () => {
-	return gulp.src(SOURCES_DIR + '/js/**/*.js')
+	return gulp.src([SOURCES_DIR + '/js/**/*.js', '!' + SOURCES_DIR + '/js/vendor/**/*.js'])
 		.pipe($.eslint())
 		.pipe($.eslint.format());
 });
@@ -174,22 +225,23 @@ gulp.task('lint', () => {
 // Inject plugin --> generate index.html file
 
 gulp.task('indexGenerate', () => {
-	return gulp.src(PUBLIC_DIR + '/_pages.html')
+
+	return localDestPathFlag ? gulp.src(SOURCES_DIR + '/static/_pages.html')
 		.pipe($.inject(
-			gulp.src([PUBLIC_DIR + '/*.html'], {read:false}), {
-				transform: function(filepath){
-					if(filepath.slice(-5) === '.html'){
-						var regName = /(\/public\/)|(\.html)/gi,
+			gulp.src([PUBLIC_DIR + '/*.html'], {read: false}), {
+				transform: function (filepath) {
+					if (filepath.slice(-5) === '.html') {
+						let regName = /(\/public\/)|(\.html)/gi,
 							fileName = filepath.replace(regName, ''),
 							cleanPath = fileName + '.html';
 
-						return '<li class="list__item"><a href="' + cleanPath + '" class="list__link">' + fileName + '</a></li>';
+						return (fileName !== '_pages') ? '<li class="list__item"><a href="' + cleanPath + '" class="list__link">' + fileName + '</a></li>' : '';
 					}
 					return inject.transform.apply(inject.transform, arguments);
 				}
 			}
 		))
-		.pipe(gulp.dest(PUBLIC_DIR))
+		.pipe(gulp.dest(PUBLIC_DIR)) : false;
 });
 
 // ----------------------------------------------------------------------------
@@ -199,20 +251,18 @@ gulp.task('indexGenerate', () => {
 import config from './config.json';
 let bundler = config.bundler;
 
-console.log(bundler);
-
-function initBundles(bundler, watch){
+function initBundles (bundler, watch) {
 
 	for (let i in bundler) {
 
 		if (bundler.hasOwnProperty(i)) {
 
-			if (bundler[i].bundler){
+			if (bundler[i].bundler) {
 				return false;
 			}
 
 			bundler[i].bundler = browserify({
-				entries: [bundler[i].filepath + bundler[i].filename],
+				entries: [SOURCES_DIR + '/js/' + bundler[i].filename],
 				cache: {},
 				packageCache: {},
 				poll: false,
@@ -229,23 +279,26 @@ function initBundles(bundler, watch){
 	}
 }
 
-function makeBundle(name, bundlename, cb){
+function makeBundle (name, bundlename, cb) {
 
-	if (typeof bundler[name] === "undefined"){
+	if (typeof bundler[name] === "undefined") {
 		console.error(`ERROR: can't find bundle ${name}`);
 		return false;
 	}
 
 	bundler[name].bundler.bundle()
-		.on('error', function(err) { console.error(err); this.emit('end'); })
-		.on('end', function(){
-			if (typeof cb === "function"){
+		.on('error', function (err) {
+			console.error(err);
+			this.emit('end');
+		})
+		.on('end', function () {
+			if (typeof cb === "function") {
 				cb();
 			}
 		})
 		.pipe(source(bundlename))
 		.pipe(buffer())
-		.pipe($.sourcemaps.init({ loadMaps: true }))
+		.pipe($.sourcemaps.init({loadMaps: true}))
 		.pipe($.sourcemaps.write('./'))
 		.pipe(gulp.dest(PUBLIC_DIR + '/js/'))
 		.pipe(reload({stream: true}));
@@ -253,8 +306,8 @@ function makeBundle(name, bundlename, cb){
 }
 
 gulp.task('js-build', () => {
-	for (let i in bundler){
-		if (bundler.hasOwnProperty(i)){
+	for (let i in bundler) {
+		if (bundler.hasOwnProperty(i)) {
 
 			console.log(`Bundling ${bundler[i].bundlename}...`);
 			let timeStart = new Date().getTime();
@@ -264,7 +317,7 @@ gulp.task('js-build', () => {
 			});
 		}
 	}
-})
+});
 
 gulp.task('js-clean', (cb) => {
 	rimraf(PUBLIC_DIR + '/js/', cb);
@@ -283,18 +336,22 @@ gulp.task('build', ['build-persistent'], () => {
 });
 
 gulp.task('web-bs', ['build-persistent'], () => {
-
-	browserSync({
-		server: {
-			baseDir: PUBLIC_DIR
-		}
-	});
+	if (localDestPathFlag) {
+		browserSync({
+			server: {
+				baseDir: PUBLIC_DIR
+			}
+		});
+	}
 
 	return initBundles(bundler, true);
 });
 
 // WEB SERVER
 gulp.task('web', () => {
+	if (!localDestPathFlag)
+		return false;
+
 	browserSync({
 		server: {
 			baseDir: PUBLIC_DIR
@@ -304,7 +361,9 @@ gulp.task('web', () => {
 
 gulp.task('watch', () => {
 	gulp.watch(['css/**'], {cwd: SOURCES_DIR + '/'}, ['serve:sass']);
-	gulp.watch(['js/**'], {cwd: SOURCES_DIR + '/'}, ['js-build']);
+	gulp.watch(['js/*.js', 'js/modules/*.js'], {cwd: SOURCES_DIR + '/'}, ['js-build']);
+	gulp.watch(['**/*.js'], {cwd: SOURCES_DIR + '/js/vendor/'}, ['serve:vendor-js']);
+	gulp.watch(['**/*.js'], {cwd: SOURCES_DIR + '/js/custom/'}, ['serve:custom-js']);
 	gulp.watch(['*.pug', 'layout/**/*.pug'], {cwd: SOURCES_DIR + '/'}, ['serve:pug']);
 	gulp.watch(['static/**'], {cwd: SOURCES_DIR + '/'}, ['serve:static']);
 	gulp.watch(['img/**/*.svg', 'img/**/*.jpg', 'img/**/*.jpeg', 'img/**/*.png', 'img/**/*.bmp'], {cwd: SOURCES_DIR + '/'}, ['serve:images']);
